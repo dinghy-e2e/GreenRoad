606d6676ce12c1387f3853b930c987183b565cc3
Dinghy-e2e <103927731+dinghy-e2e@users.noreply.github.com>
2023-06-04 17:35:18 +0300

    Update output.tf

--- GreenRoad-Home-Assignment/output.tf
+++ GreenRoad-Home-Assignment/output.tf
@@ -7,3 +7,15 @@ output "private_pem" {
   value     = tls_private_key.key.private_key_pem
   sensitive = true
 }
+
+output "public_web_app" {
+  value = "${aws_instance.ec2.public_dns}:31234 2048-web app"
+}
+
+output "public_grafana" {
+  value = "${aws_instance.ec2.public_dns}:30002 grafana"
+}
+
+output "public_prometheus" {
+  value = "${aws_instance.ec2.public_dns}:30001 prometheus"
+}
