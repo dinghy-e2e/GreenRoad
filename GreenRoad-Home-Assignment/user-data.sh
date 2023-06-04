b8c3030bbcf5a376b97b0ec1f2da1d1ed1e7aff3
Dinghy-e2e <103927731+dinghy-e2e@users.noreply.github.com>
2023-06-04 17:37:27 +0300

    Update user-data.sh

--- GreenRoad-Home-Assignment/user-data.sh
+++ GreenRoad-Home-Assignment/user-data.sh
@@ -8,7 +8,7 @@ fi
 # Output log
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-# Install k8s
+#//// Install k8s ////#
 
 apt update && apt upgrade -y
 
@@ -17,31 +17,25 @@ curl -sfL https://get.k3s.io | sh -
 # export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 # Install helm
+# wget https://get.helm.sh/helm-v3.9.3-linux-amd64.tar.gz
+# tar xvf helm-v3.9.3-linux-amd64.tar.gz
+# mv linux-amd64/helm /usr/local/bin
+# rm helm*
 
-wget https://get.helm.sh/helm-v3.9.3-linux-amd64.tar.gz
-
-tar xvf helm-v3.9.3-linux-amd64.tar.gz
-
-mv linux-amd64/helm /usr/local/bin
-
-rm helm*
-
-# Install Services (Prometheus, Node exporter, Grafana, 2048 game)
+#//// Install Services (Prometheus, Node exporter, Grafana, 2048 game) ////#
 
 kubectl create ns greenroad
 
 
-# 2048
+#//// 2048 ////#
 git clone https://github.com/dinghy-e2e/GreenRoad.git
 
 kubectl apply -f /GreenRoad/GreenRoad-Home-Assignment/OPS/2048.yaml
 
-# Monitoring install 
+#//// Monitoring install ///#
 
-# cheking url
+# replacing url
 public_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-
-# fix url
 sed -i "s/public_ip/$public_ip/g" /GreenRoad/GreenRoad-Home-Assignment/OPS/prometheus-stack.yaml
 
 kubectl apply -f /GreenRoad/GreenRoad-Home-Assignment/OPS/prometheus-stack.yaml
